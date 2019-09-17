367cf493f521cd10d2dfc125b5f30b93b9452219
chadwick <cekeller6121@yahoo.com>
2019-09-17 06:51:21 -0500

    changing js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,34 @@
 console.log("scripts connected");
 
-var total;
+var billAmount = document.getElementById("billAmount").value;
+
+var tipAmount = document.getElementById("tipAmount").value / 100;
 
 function calculateTip() {
 
-  var billAmount = document.getElementById("billAmount").value;
+  // var billAmount = document.getElementById("billAmount").value;
+  //
+  // var tipAmount = document.getElementById("tipAmount").value / 100;
 
-  var tipAmount = document.getElementById("tipAmount").value / 100;
+  // if (billAmount || tipAmount == 0) {
+  //   console.log("bill amount or tip amount equal 0");
+  //   // alert("Please enter both a Bill amount and Tip amount");
+  // } else {
+  //
+  //
+  //
+  // }
 
-  if (billAmount || tipAmount == 0) {
-    console.log("bill amount or tip amount equal 0");
-    // alert("Please enter both a Bill amount and Tip amount");
-  } else {
-
-    var total = (billAmount * tipAmount);
-    alert(Math.round(total));
-
-  }
+  var total = (billAmount * tipAmount);
+  alert(Math.round(total));
 
 };
 
 document.getElementById("submitButton").onclick = function() {
 
+  // var displayField = document.getElementById("displayField").innerHTML = total;
+
   calculateTip();
+
 
 };
