dde25cf13b0ac7152d2006842968d019fbd9a929
chadwick <cekeller6121@yahoo.com>
2019-09-17 20:06:34 -0500

    changed computation and display removed junk

--- scripts.js
+++ scripts.js
@@ -1,11 +1,5 @@
 console.log("scripts connected");
 
-// var billAmount = document.getElementById("billAmount").value;
-//
-// var tipAmount = document.getElementById("tipAmount").value / 100;
-//
-// var total;
-
 function calculateTip() {
 
   var billAmount = document.getElementById("billAmount").value;
@@ -14,14 +8,11 @@ function calculateTip() {
 
   var total = (billAmount * tipAmount);
 
-  // alert(Math.ceil(total));
-
   var roundedAmt = Math.ceil(total);
-  var billTotal = (parseFloat(roundedAmt) + parseFloat(billAmount));
+  var billTotal = (Number(roundedAmt) + Number(billAmount));
+  var billTotal = billTotal.toFixed(2);
 
   document.getElementById("displayField").innerHTML = "Bill: $" + billAmount + "<br>" + "Tip: $" + roundedAmt + "<br>" + "Total: $" + billTotal + "<br><br>";
-
-  return false;
 
 };
 
